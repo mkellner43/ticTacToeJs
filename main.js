0864b487c48e00c1c2912f188064d2465b932410
mkellner43 <matthew.kellner@outlook.com>
2022-09-28 14:44:50 -0500

    updated UI and built reset and play again buttons

--- main.js
+++ main.js
@@ -21,16 +21,15 @@ const createPlayer = (name, symbol) => {
 };
 
 const game = (players) => {
-  let board = gameBoard();
+  let board = gameBoard().board;
   const winningCombos = [
    [0, 1, 2], [3, 4, 5], [6, 7, 8],
    [0, 3, 6], [1, 4, 7], [2, 5, 8],
    [6, 4, 2], [8, 4, 0]
   ];
   const [player1, player2] = players
-  let options = Array.from(document.querySelectorAll('.box'));
   let message = document.getElementById('info');
-  message.textContent =  `${player1.name} it's your turn!`;
+  message.textContent =  `${player1.name}, it's your turn!`;
   let playerOnePositions = [];
   let playerTwoPositions = [];
   let playerOneWin = [];
@@ -38,13 +37,12 @@ const game = (players) => {
   let turn =  0;
   let tie = false;
 
-
-  function checkWin(options) {
-    for (let i = 0; i < options.length; i++) {
-      if (options[i].textContent == 'X' && !playerOnePositions.includes(options[i].id)) {
-        playerOnePositions.push(options[i].id);
-      } else if (options[i].textContent == 'O' && !playerTwoPositions.includes(options[i].id)) {
-        playerTwoPositions.push(options[i].id);
+  function checkWin(board) {
+    for (let i = 0; i < board.length; i++) {
+      if (board[i].textContent == 'X' && !playerOnePositions.includes(board[i].id)) {
+        playerOnePositions.push(board[i].id);
+      } else if (board[i].textContent == 'O' && !playerTwoPositions.includes(board[i].id)) {
+        playerTwoPositions.push(board[i].id);
       }
     }
     winningCombos.forEach((combo) => {
@@ -62,38 +60,51 @@ const game = (players) => {
       playerTwoWin.push(playerTwoScore);
     });
   }
+
   function annouceWinner(player) {
     message.textContent =`${player.name} wins!`;
     return resetGame();
   }
 
   function resetGame() {
-    let getResetBtn = document.getElementById('reset')
-    if(getResetBtn == null){
-      let resetBtn = document.createElement('button');
-      resetBtn.textContent = 'Play again!';
-      resetBtn.setAttribute('id', 'reset')
-      message.appendChild(resetBtn);
-      reset();
-    } else { return };
-  }
+    let playAgain = document.createElement('button');
+    playAgain.innerHTML = 'Play Again!';
+    playAgain.setAttribute('id', 'playAgain');
+    message.appendChild(playAgain)
+
+    let reset = document.createElement('button');
+    reset.textContent = 'Reset';
+    reset.setAttribute('id', 'reset');
+    message.appendChild(reset);
+
+    reset.addEventListener('click', () => {
+      window.location.reload();
+    });
+    
+    playAgain.addEventListener('click', () => {
+      let gameGrid = document.getElementById('grid')
+      gameGrid.innerHTML = ""
+      game(players);
+    })
+  };
+
   //listen for player click
-  for(let i = 0; i<options.length; i++) {
-    options[i].addEventListener('click', function(e){
+  for(let i = 0; i<board.length; i++) {
+    board[i].addEventListener('click', function(e){
       if(playerOneWin.includes(3) || playerTwoWin.includes(3)) {return resetGame()};
       if(turn <= 8) {
       if(turn % 2 == 0 && this.textContent == ''){
         this.textContent = player1.symbol
         turn++;
-        checkWin(options);
+        checkWin(board);
         if(playerOneWin.includes(3)){ return annouceWinner(player1) }
-          else{message.textContent = `${player2.name} it's your turn!`};
+          else{message.textContent = `${player2.name}, it's your turn!`};
       } else if(turn % 2 == 1 && this.textContent == '') {
         this.textContent = player2.symbol;
         turn++;
-        checkWin(options)
+        checkWin(board)
         if(playerTwoWin.includes(3)){ return annouceWinner(player2) }
-          else{ message.textContent = `${player1.name} it's your turn!` };
+          else{ message.textContent = `${player1.name}, it's your turn!` };
         }
       } if(turn == 9){
         message.textContent = "Its a tie!"
@@ -101,12 +112,8 @@ const game = (players) => {
       }
     })
   }
-
-  function reset(){
-    let reset = document.getElementById('reset')
-    reset.addEventListener('click', function(e) {
-    window.location.reload();
-    });
+  return{
+    game
   }
 };
 
@@ -144,9 +151,9 @@ const createPlayerForm = () =>{
         let player = createPlayer(name, symbol)
         players.push(player)
         formInput.value = '';
-        formLabel.innerHTML = 'Player 2 name '
+        formLabel.innerHTML = 'Player 2 name ';
       }else{
-        formError.innerHTML = "Please enter at least one character that hasnt been selected"
+        formError.innerHTML = "Please enter at least one character that hasn't been selected"
       }
       if(players.length == 2){
         form.removeChild(formLabel);
@@ -156,19 +163,18 @@ const createPlayerForm = () =>{
         game(players)
       }
     })
-}
+};
 
 const startAGame = (() => {
-  let displayButton = document.getElementById('title')
-  let startButton = document.createElement('button')
-  startButton.innerHTML = 'Start A Game!'
-  startButton.setAttribute('id', 'start')
+  let displayButton = document.getElementById('title');
+  let startButton = document.createElement('button');
+  startButton.innerHTML = 'Start A Game!';
+  startButton.setAttribute('id', 'start');
   displayButton.appendChild(startButton);
 
   startButton.addEventListener('click', (e)=> {
     e.preventDefault();
     createPlayerForm();
-    displayButton.removeChild(startButton)
+    displayButton.removeChild(startButton);
   })
 })();
-;
